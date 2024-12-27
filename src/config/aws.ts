e3407f39be361db74a3d4b73a7dc14a98e97b097
SAC <sachin@crftech360.com>
2024-12-27 11:31:20 +0530

    aws-updated

--- src/config/aws.ts
+++ src/config/aws.ts
@@ -1,8 +1,8 @@
 export const awsConfig = {
-  region: process.env.VITE_AWS_REGION || '', // Provide default values or validation if necessary
+  region: import.meta.env.VITE_AWS_REGION || '', // Provide default values or validation if necessary
   credentials: {
-    accessKeyId: process.env.VITE_AWS_ACCESS_KEY_ID || '',
-    secretAccessKey: process.env.VITE_AWS_SECRET_ACCESS_KEY || '',
+    accessKeyId: import.meta.env.VITE_AWS_ACCESS_KEY_ID || '',
+    secretAccessKey: import.meta.env.VITE_AWS_SECRET_ACCESS_KEY || '',
   },
-  bucketName: process.env.VITE_AWS_BUCKET_NAME || '',
+  bucketName: import.meta.env.VITE_AWS_BUCKET_NAME || '',
 };
